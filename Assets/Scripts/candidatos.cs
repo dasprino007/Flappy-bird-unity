53bba70f39eb36ee5e4ff585b189e040ea5843cc
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-27 11:22:14 -0300

    modificar variaveis e colisões dos canditatos

--- Assets/Scripts/candidatos.cs
+++ Assets/Scripts/candidatos.cs
@@ -6,10 +6,23 @@ public class candidatos : MonoBehaviour
 {
     [SerializeField]    
     private float velocidade = 0.8f;
+    [SerializeField]
+    private float tempoparadeletar;
+
     // Update is called once per frame
     void Update()
     {
         this.transform.Translate(Vector3.right * velocidade * Time.deltaTime);
         this.transform.Translate(Vector3.up * Random.Range(-0.003f, 0.003f));
     }
+
+    // isso daqui é para deletar o obstaculo,
+    // quando sair da camera mas ele deleta no meio da camera,
+    // mas se colocar um tempo ele não vai deletar na camera e não sei como consertar.
+    private void OnBecameInvisible()
+    {
+        Debug.Log("invisível");
+        Destroy(this.gameObject, tempoparadeletar);
+    }
 }
+
